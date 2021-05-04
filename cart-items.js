1886b7ea67de66eda5d59e6e461fa5a32efe3a8e
Maggie Oudsema <maggieoudsema@gmail.com>
2021-05-03 20:45:14 -0400

    modified files

--- cart-items.js
+++ cart-items.js
@@ -1,49 +1,60 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.use(express.json());
 
-const items = [
+let items=[
   {
-    id: items.array + 1,
+    id: 1,
     product: "Multi vitamin for her",
     price: 19.99,
     quantiy: 1,
   },
   {
-    id: items.array + 1,
+    id: 2,
     product: "Honey Crisp Apples",
     price: 2.99,
     quantiy: 8,
   },
   {
-    id: items.array + 1,
+    id: 3,
     product: "Whole Milk",
     price: 4.99,
     quantiy: 2,
   },
   {
-    id: items.array + 1,
+    id: 4,
     product: "Bananas",
     price: 0.4,
     quantiy: 6,
   },
 ];
 
+//get /cart-items
 router.get("/cart-items", (req, res) => {
-  res.json(items);
+  res.status(200).json(items);
 });
+
+
+router.get("/cart-items/id", (req, res) =>{
+  let cartItems = items.find()
+  res.items.id;
+  res.status(200).json(items);
+} )
+
 
 //accept POST request at URI: /students
 router.post("/cart-items", (req, res) => {
     console.log(req.body)
-    items.push({
-        id: items.length+1,
-        product:req.body.product,
-        price: req.body.price,
-        quantiy: req.body.quanity
-    })
-  res.json("adding a item...");
+    let itemAdded = {
+      id: items.length + 1,
+      product: req.body.product,
+      price: req.body.price,
+      quantiy: req.body.quantiy,
+    };
+    items.push(itemAdded);
+    res.status(201).json(itemAdded);
 });
 
 module.exports = router;
